b40b67112ecc95934175a0c59fc5f1c5895ebfd9
Alex Rivie <rivie.alex@gmail.com>
2019-11-22 16:59:10 -0500

    switch to white ui

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ var draw = Draw()
 
 var hexarr = []
 
-// takes in an array called hex that the Hex library will operate on
+// takes in an integer array of hexagram codes that the Hex library will operate on
 export function update(arr, reset=false) {
 	document.querySelector('#hexagram').innerHTML = draw.linesAsCss(arr);
 	if (hex.isComplete(arr) || reset==true) {
@@ -17,30 +17,30 @@ export function update(arr, reset=false) {
 			document.querySelector('#secondary').innerHTML = draw.linesAsCss(hex.secondary(arr))
 			document.querySelector('#primary-result').innerHTML = hex.lookup(hex.primary(arr))
 			document.querySelector('#secondary-result').innerHTML = hex.lookup(hex.secondary(arr))
+			document.querySelector('#changingvisual').style.visibility = 'visible'
+			if (reset==true) {
+				document.querySelector('#changingvisual').style.visibility = 'hidden'
+			}
+
 		}
 	}
 }
-
 
 window.addEventListener('load',
 	function() {
 		let cast = document.getElementById('cast')
 		let reset = document.getElementById('reset')
 		let hexagram = document.getElementById('hexagram')
-		let result = document.getElementById('result')
 		let help = document.getElementById('help')
-		let visuals = document.getElementById('visuals')
-		let yinYang = document.getElementById('yinYang')
+		let mainvisual = document.getElementById('mainvisual')
 
-		yinYang.onclick = function() {
+		mainvisual.onclick = function() {
 			if (hex.isComplete(hexarr)) {
-				console.log('entering')
 				let alertmsg = document.createElement('div');
 				alertmsg.className = "alert alert-warning"
-				alertmsg.innerHTML = `<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    			<span aria-hidden="true">&times;</span>
-  				</button>`
-				document.getElementById('alert').insertAdjacentElement('beforebegin', title)
+				alertmsg.innerHTML = `<button type="button" class="close" data-dismiss="alert"`
+										+ `aria-label="Close"><span aria-hidden="true">&times;</span></button>`
+				//document.getElementById('alert').insertAdjacentElement('beforebegin', alertmsg)
 				setTimeout(() => alertmsg.remove(), 1000);
 			}
 			hexarr = hex.addLine(hexarr)
@@ -51,9 +51,8 @@ window.addEventListener('load',
 			hexarr = []
 			update(hexarr, true)
 		}
+
 		hexagram.onclick = function() {
-			// TODO: make a clickable link
-			// or information about the hex
 		}
 	}
 )
